6aa9a4dbcfb070dffeb89b236fbcfad1774f31c9
Deyvid Rannyere <deyran@gmail.com>
2024-05-31 17:45:07 -0300

    Update

---
+++ Acessando-SQLite/AgendaApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AgendaApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Acessando-SQLite/AgendaApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AgendaApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
