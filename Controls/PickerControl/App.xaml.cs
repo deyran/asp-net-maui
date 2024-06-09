f61e3472290305663551b0d8240fd720fa09b8c9
Deyvid Rannyere <deyran@gmail.com>
2024-06-09 18:50:53 -0300

    Picker

---
+++ Controls/PickerControl/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PickerControl
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Controls/PickerControl/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PickerControl
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
